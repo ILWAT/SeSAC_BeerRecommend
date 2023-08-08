cd4aae314c97061f08d5c4f759297a9caa01550a
J.H. Moon <moonjh587@naver.com>
2023-08-08 12:33:30 +0900

    Initial Commit

---
+++ SeSAC_BeerRecommend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_BeerRecommend
+//
+//  Created by 문정호 on 2023/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
